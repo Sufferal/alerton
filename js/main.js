5085acde00997d29a9685406d8987780285b9f13
Ciprian <botnari_ciprian@mail.ru>
2023-05-25 17:34:36 +0300

    Add initial modal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log('yo');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+const submitBtn = document.querySelector(".btn-submit");
+
+// Open modal
+const openModal = function () {
+  modal.classList.add("open");
+  modal.classList.remove("hidden");
+  overlay.classList.add("open");
+  overlay.classList.remove("hidden");
+};
+
+openModalBtn.addEventListener("click", openModal);
+
+// Close modal
+const closeModal = function () {
+  modal.classList.remove("open");
+  modal.classList.add("hidden");
+  overlay.classList.remove("open");
+  overlay.classList.add("hidden");
+};
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && modal.classList.contains("open")) {
+    closeModal();
+  }
+});
+
+closeModalBtn.addEventListener("click", closeModal);
+submitBtn.addEventListener("click", closeModal);
